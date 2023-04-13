1e6575ee1ea846a2f99dc05b43d4d64943883175
Trinisia Fortune <trinisiafortune@Trinisias-MBP.lan>
2023-04-13 16:24:38 -0400

    nav bar added/routes done

--- app/routes.py
+++ app/routes.py
@@ -5,14 +5,23 @@ from flask import render_template
 
 #  specify routes:
 
-@app.route('/mydecks')
+@app.route('/')
 def myDecks():
     """displays/returns list of decks upon signing in"""
+
+
     return render_template('mydecks.html')
+    # when adding keys and values to the return, you will be using that key to access values in html
+    # to access, use {{ ____ }} in html file. to create for loops, use {% ____ %} in html file
     
-    
-# @app.route('/flashcard')
-# def flashCard():
-#     """returns empty flash card?"""
-#     return 
+@app.route('/flashcard')
+def flashCard():
+    """returns empty flash card?"""
+    return render_template('flashcard.html')
+
+
+@app.route('/practice')
+def practice():
+    """returns completed flash card"""
+    return render_template('practice.html')
 
